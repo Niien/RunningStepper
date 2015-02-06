02ad42bc367c80fcd87b7df7ab28c52f853076b0
Niien <niien@gmail.com>
2015-02-07 02:05:18 +0800

    微調

--- OnlineFightViewController.m
+++ OnlineFightViewController.m
@@ -10,6 +10,7 @@
 
 @interface OnlineFightViewController (){
     LoginVC *LogIn;
+    NSMutableArray *EnemyArray;
 }
 
 @end
@@ -19,7 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    //使用delegate的方法
+
 
 }
 
@@ -30,6 +31,8 @@
 
 -(void)viewWillAppear:(BOOL)animated{
     
+    EnemyArray = [NSMutableArray new];
+    //
     if ([PFUser currentUser]) {
         //
         NSLog(@"DID LOGIN");
@@ -38,25 +41,30 @@
         [self getEnemy];
     }
 }
-
+#pragma mark 判斷是否已存在使用者
 - (void) DidExistInFightUserOrNot{
+    //從Plist抓資料下來
     NSArray *teamArray = [[myPlist shareInstanceWithplistName:@"team"] getDataFromPlist];
-    NSLog(@"team:%@",teamArray);
+    NSLog(@"Plist_team:%@",teamArray);
+    //取得
     PFQuery *checkID = [[PFQuery alloc]initWithClassName:@"FightUser"];
     PFUser *user = [PFUser currentUser];
+    //確認是否有重複
     [checkID whereKey:@"username" equalTo:user.username];
     [checkID findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        //若不為空值 便清空 再置入
         if([objects count] != 0){
             for(PFObject *obj in objects){
                 [obj deleteInBackground];
                 NSLog(@"remove");
             }
         }
-            PFObject *object = [[PFObject alloc]initWithClassName:@"FightUser"];
-            [object setObject:teamArray forKey:@"Team"];
-            [object setObject:user.username forKey:@"username"];
-            [object saveInBackground];
-            NSLog(@"got");
+        //
+        PFObject *object = [[PFObject alloc]initWithClassName:@"FightUser"];
+        [object setObject:teamArray forKey:@"Team"];
+        [object setObject:user.username forKey:@"username"];
+        [object saveInBackground];
+        NSLog(@"got");
     }];
 }
 
@@ -66,12 +74,27 @@
     //
     [getID findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
         int i = arc4random()%objects.count;
-        [objects[i]valueForKey:@"Team"];
+        EnemyArray = [objects[i]valueForKey:@"Team"];
         NSLog(@"Enemy:%@",[objects[i]valueForKey:@"Team"]);
+//        [EnemyArray addObjectsFromArray:[objects[i]valueForKey:@"Team"]];
+        //把東西抓下來需要一段時間
     }];
-
 }
 
+- (IBAction)Back:(id)sender {
+    //暫放
+    NSLog(@"EnemyArray:%@",EnemyArray);
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+
+
+- (void) viewWillDisappear:(BOOL)animated{
+    //Cleaner
+    EnemyArray = [NSMutableArray new];
+}
 /*
 #pragma mark - Navigation
 
